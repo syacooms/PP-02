426948a2ffd9d6efd1b94bafe4bd19b2170eeac1
syacooms <syacooms@gmail.com>
2022-01-07 16:56:21 +0900

    Feature : mongoose 연결

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,42 @@
+require('dotenv').config();
 import express, { Request, Response } from 'express';
 const app = express();
+const mongoose = require('mongoose');
+const Image = require('../models/Image');
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected.');
+
+    interface MulterRequest extends Request {
+      file: any;
+    }
+
+    //client 업로드 이미지 조회
+    app.use('/uploads', express.static('upload'));
+
+    app.post(
+      '/upload',
+      upload.single('image'),
+      async (req: Request, res: Response) => {
+        const image = await new Image({
+          key: req.file?.filename,
+          originalFileName: req.file?.originalname,
+        }).save();
+        res.json(image);
+      }
+    );
+  })
+  .catch((err: any) => console.log(err));
+
+app.get('/images', async (req, res) => {
+  const images = await Image.find();
+  res.json(images);
+});
 //multer
 const multer = require('multer');
 const { v4: uuid } = require('uuid');
@@ -37,18 +73,6 @@ const upload = multer({
   storage: storage,
   fileFilter: imageFilter,
   limits: { fileSize: 1024 * 512 },
-});
-
-interface MulterRequest extends Request {
-  file: any;
-}
-
-//client 업로드 이미지 조회
-app.use('/uploads', express.static('upload'));
-
-app.post('/upload', upload.single('image'), (req: Request, res: Response) => {
-  console.log((req as MulterRequest).file);
-  res.json((req as MulterRequest).file);
 });
 
 app.get('/', (req: Request, res: Response) => {
